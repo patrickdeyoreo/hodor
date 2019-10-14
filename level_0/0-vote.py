5cff8d300df3bcab657618136b3a0d5e3b9aa6fa
patrickdeyoreo <pdeyoreo@gmail.com>
2019-10-13 19:30:54 -0700

    Grouped script execution under 'if __name__ == "__main__":'

--- level_0/0-vote.py
+++ level_0/0-vote.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """ Vote for my ID
 """
-from requests import exceptions, request
 from sys import argv, exit as sysexit, stderr
 from time import sleep
+from requests import exceptions, request
 
 
 def confirm(prompt="[Y/n] "):
@@ -19,64 +19,66 @@ def confirm(prompt="[Y/n] "):
             pass
 
 
-url = 'http://158.69.76.135/level0.php'
+if __name__ == '__main__':
 
-headers = {
-    'Host': '158.69.76.135',
-    'Content-Type': 'application/x-www-form-urlencoded',
-    'Referer': 'http://158.69.76.135/level0.php',
-}
+    url = 'http://158.69.76.135/level0.php'
 
-data = {
-    'id': '801',
-    'holdthedoor': 'Submit',
-}
+    headers = {
+        'Host': '158.69.76.135',
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Referer': 'http://158.69.76.135/level0.php',
+    }
 
-usage = f"{argv[0].split('/')[-1]} count"
+    data = {
+        'id': '801',
+        'holdthedoor': 'Submit',
+    }
 
-if len(argv) != 2:
-    print(f"usage: {usage}", file=stderr)
-    sysexit(2)
+    usage = f"{argv[0].split('/')[-1]} count"
 
-try:
-    count = int(argv[1])
-except ValueError:
-    print(f"invalid count: {argv[1]}", file=stderr)
-    print(f"usage: {usage}", file=stderr)
-    sysexit(1)
-
-try:
-    if not confirm(f"Submit {count} vote{'s' * (count != 1)}? [Y/n] "):
-        print("Aborting.")
-        sysexit(0)
+    if len(argv) != 2:
+        print(f"usage: {usage}", file=stderr)
+        sysexit(2)
 
     try:
-        request("GET", url=url)
-    except exceptions.ConnectionError:
-        print("\nUnable to connect.")
+        count = int(argv[1])
+    except ValueError:
+        print(f"invalid count: {argv[1]}", file=stderr)
+        print(f"usage: {usage}", file=stderr)
         sysexit(1)
 
-    for vote in range(1, count + 1):
-        try:
-            print(f"Submitting vote #{vote}...", end="")
-            request("POST", url=url, headers=headers, data=data)
-            print()
-        except exceptions.ConnectionError:
-            print("\nConnecting...", end="")
-            for reconnect in range(6):
-                sleep(10)
-                try:
-                    request("GET", url=url)
-                except exceptions.ConnectionError:
-                    if reconnect == 6:
-                        print("\nUnable to connect.")
-                        sysexit(1)
-                else:
-                    print()
-                    break
-except KeyboardInterrupt:
-    print("\nReceived interrupt.")
-    sysexit(130)
+    try:
+        if not confirm(f"Submit {count} vote{'s' * (count != 1)}? [Y/n] "):
+            print("Aborting.")
+            sysexit(0)
 
-print("Done.")
-sysexit(0)
+        try:
+            request("GET", url=url)
+        except exceptions.ConnectionError:
+            print("\nUnable to connect.")
+            sysexit(1)
+
+        for vote in range(1, count + 1):
+            try:
+                print(f"Submitting vote #{vote}...", end="")
+                request("POST", url=url, headers=headers, data=data)
+                print()
+            except exceptions.ConnectionError:
+                print("\nConnecting...", end="")
+                for reconnect in range(6):
+                    sleep(10)
+                    try:
+                        request("GET", url=url)
+                    except exceptions.ConnectionError:
+                        if reconnect == 6:
+                            print("\nUnable to connect.")
+                            sysexit(1)
+                    else:
+                        print()
+                        break
+    except KeyboardInterrupt:
+        print("\nReceived interrupt.")
+        sysexit(130)
+
+    print("Done.")
+    sysexit(0)
--- level_1/1-vote.py
+++ level_1/1-vote.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """ Vote for my ID
 """
-from requests import exceptions, sessions
 from sys import argv, exit as sysexit, stderr
 from time import sleep
+from requests import exceptions, sessions
 
 
 def confirm(prompt="[Y/n] "):
@@ -19,73 +19,75 @@ def confirm(prompt="[Y/n] "):
             pass
 
 
-url = 'http://158.69.76.135/level1.php'
+if __name__ == '__main__':
 
-headers = {
-    'Host': '158.69.76.135',
-    'Content-Type': 'application/x-www-form-urlencoded',
-    'Referer': 'http://158.69.76.135/level1.php',
-}
+    url = 'http://158.69.76.135/level1.php'
 
-data = {
-    'id': '801',
-    'holdthedoor': 'Submit',
-}
+    headers = {
+        'Host': '158.69.76.135',
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Referer': 'http://158.69.76.135/level1.php',
+    }
 
-usage = f"{argv[0].split('/')[-1]} count"
+    data = {
+        'id': '801',
+        'holdthedoor': 'Submit',
+    }
 
-if len(argv) != 2:
-    print(f"usage: {usage}", file=stderr)
-    sysexit(2)
+    usage = f"{argv[0].split('/')[-1]} count"
 
-try:
-    count = int(argv[1])
-except ValueError:
-    print(f"invalid count: {argv[1]}", file=stderr)
-    print(f"usage: {usage}", file=stderr)
-    sysexit(1)
+    if len(argv) != 2:
+        print(f"usage: {usage}", file=stderr)
+        sysexit(2)
 
-with sessions.Session() as session:
     try:
-        if not confirm(f"Submit {count} vote{'s' * (count != 1)}? [Y/n] "):
-            print("Aborting.")
-            sysexit(0)
+        count = int(argv[1])
+    except ValueError:
+        print(f"invalid count: {argv[1]}", file=stderr)
+        print(f"usage: {usage}", file=stderr)
+        sysexit(1)
 
-        print("Initiating session...", end="")
+    with sessions.Session() as session:
         try:
-            session.get(url)
-        except exceptions.ConnectionError:
-            print("\nUnable to connect.")
-            sysexit(1)
+            if not confirm(f"Submit {count} vote{'s' * (count != 1)}? [Y/n] "):
+                print("Aborting.")
+                sysexit(0)
 
-        data['key'] = session.cookies['HoldTheDoor']
-        headers['Cookie'] = f"HoldTheDoor={data['key']}"
-        print(f"\nSession initiated (key: {data['key']})")
-
-        for vote in range(1, count + 1):
+            print("Initiating session...", end="")
             try:
-                print(f"Submitting vote #{vote}...", end="")
-                session.post(url=url, headers=headers, data=data)
-                print()
+                session.get(url)
             except exceptions.ConnectionError:
-                print("\nReconnecting...", end="")
-                for reconnect in range(6):
-                    sleep(10)
-                    try:
-                        session.get(url)
-                    except exceptions.ConnectionError:
-                        if reconnect == 6:
-                            print("\nUnable to reconnect.")
-                            sysexit(1)
-                    else:
-                        data['key'] = session.cookies['HoldTheDoor']
-                        headers['Cookie'] = f"HoldTheDoor={data['key']}"
-                        print(f"\nSession continued (key: {data['key']})")
-                        break
-    except KeyboardInterrupt:
-        print("\nReceived interrupt.")
-        session.close()
-        sysexit(130)
+                print("\nUnable to connect.")
+                sysexit(1)
 
-print("Done.")
-sysexit(0)
+            data['key'] = session.cookies['HoldTheDoor']
+            headers['Cookie'] = f"HoldTheDoor={data['key']}"
+            print(f"\nSession initiated (key: {data['key']})")
+
+            for vote in range(1, count + 1):
+                try:
+                    print(f"Submitting vote #{vote}...", end="")
+                    session.post(url=url, headers=headers, data=data)
+                    print()
+                except exceptions.ConnectionError:
+                    print("\nReconnecting...", end="")
+                    for reconnect in range(6):
+                        sleep(10)
+                        try:
+                            session.get(url)
+                        except exceptions.ConnectionError:
+                            if reconnect == 6:
+                                print("\nUnable to reconnect.")
+                                sysexit(1)
+                        else:
+                            data['key'] = session.cookies['HoldTheDoor']
+                            headers['Cookie'] = f"HoldTheDoor={data['key']}"
+                            print(f"\nSession continued (key: {data['key']})")
+                            break
+        except KeyboardInterrupt:
+            print("\nReceived interrupt.")
+            session.close()
+            sysexit(130)
+
+    print("Done.")
+    sysexit(0)
